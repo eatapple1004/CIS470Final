beba2fd84153bdcd61723f0407c8a3388319b756
YongjunLee <46211947+eatapple1004@users.noreply.github.com>
2024-04-05 02:57:09 -0400

    Start CIS470 Final Project

---
+++ src/js/chat.js
@@ -0,0 +1,58 @@
+"use strict"
+
+const socket = io();
+
+const nickName = document.querySelector("#nickname");
+const chatList = document.querySelector(".chatting-list");
+const chatInput = document.querySelector(".chatting-input");
+const sebdButton = document.querySelector(".send-button");
+const displayContainer = document.querySelector(".display-container");
+
+
+chatInput.addEventListener("keypress", (event) => {
+    if(event.keyCode === 13) { // when press enter
+        send();
+    }
+})
+
+function send() {
+    const param = {
+        name: nickName.value,
+        msg: chatInput.value
+    }
+    socket.emit("chatting", param)
+    chatInput.value = "";
+}
+
+sebdButton.addEventListener("click", send)
+
+
+
+
+socket.on("chatting", (data) => {
+    const { name, msg, time } = data;
+    const item = new LiModel(name, msg, time);
+    item.makeLi();
+    displayContainer.scrollTo(0, displayContainer.scrollHeight)
+})
+
+function LiModel(name ,msg, time) {
+    this.name = name;
+    this.msg = msg;
+    this.time = time;
+
+    this.makeLi = () => {
+        const li = document.createElement("li");
+        li.classList.add(nickName.value === this.name ? "sent" : "received")
+        const dom = `<span class = "profile">
+                        <span class = "user"> ${this.name} </span>
+                        <img class = "image" src = "https://picsum.photos/50/50" alt = "any">
+                    </span>
+                    <span class = "message"> ${this.msg} </span>
+                    <span class = "time"> ${this.time} </span>`;
+
+        li.innerHTML = dom;
+        chatList.appendChild(li);
+
+    }
+}
